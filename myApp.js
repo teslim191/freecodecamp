cb4ceb9f9a832bc10467910a423c52a1349923e3
Teslim <teslimjimoh191@gmail.com>
2022-04-29 16:10:32 +0100

    added hello world

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World in console')
-    console.log("Hello World")
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World in console')
+//     console.log("Hello World")
+// })
 
-app.listen(100, ()=>console.log('server is running on port 100'))
+console.log("Hello World")

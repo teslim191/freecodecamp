75fe3ba20f8212e0e07954027bf20dd6bf9b42d0
Teslim <teslimjimoh191@gmail.com>
2022-04-29 15:35:51 +0100

    added hello world

---
+++ myApp.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+console.log("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;

f2b95fe9d72d4f897ab2820c42a6eea05184391b
Teslim <teslimjimoh191@gmail.com>
2022-04-30 11:34:37 +0100

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
 
-app.get('/', (req, res) => {
-    res.send('Hello Express')
-    
+dotenv.config({ path: "./config/config.env" });
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+app.get("/json", (req, res) => {
+  // if (process.env.MESSAGE_STYLE == "uppercase"){
+  //     res.json({"message":"HELLO JSON"})
+  // }
+  // else{
+  //     res.json({"message":"hello json"})
+  // }
+  process.env.MESSAGE_STYLE == "uppercase"
+    ? res.json({ message: "HELLO JSON" })
+    : res.json({ message: "hello json" });
+});
+
+
+app.get('/:word/echo', (req, res)=> {
+    res.json({echo: 'word'});
+    console.log(req.params.word)
 })
 
-console.log("Hello World")
+
+
+app.listen(100, () => console.log("Server is running on port 100"));
